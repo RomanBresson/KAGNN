3a42214a59fb07d7fb064b4e747299c584be5f75
Giannis Nikolentzos <giannisnik@hotmail.com>
2024-06-29 13:46:42 +0300

    Create utils.py

---
+++ graph_regression/utils.py
@@ -0,0 +1,16 @@
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss >= (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
