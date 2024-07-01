96a4fc5e08aa9babdc938f71bc3dba210ae48617
George Panagopoulos <georgepanagopoulos5@yahoo.gr>
2024-07-01 17:45:14 +0200

    Update optuna_node_classification_mlp.py

--- node_classification/optuna_node_classification_mlp.py
+++ node_classification/optuna_node_classification_mlp.py
@@ -120,6 +120,9 @@ def main():
     skip = True
     n_trials = 100
 
+    if not os.path.exists('data'):
+        os.makedirs('data')
+      
     set_seed(1)
     for dataset_name in ['Cora','CiteSeer', 'Actor','Texas','Cornell','Wisconsin', 'ogbn-arxiv']: 
         for conv_type in ['gin','gcn']:                     
