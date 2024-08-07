3c258d022e67fd965a96e82e6b5221f4e96c76ef
RomanBresson <roman.bresson.5@gmail.com>
2024-08-07 16:25:01 +0200

    Added a script for all experiments to be run

---
+++ graph_classification/scripts/all_expes.sh
@@ -0,0 +1,9 @@
+for DATASET in MUTAG PROTEINS_full IMDB-B IMDB-M DD ENZYMES NCI1
+do
+    for MODEL in mlp fastkan kan
+    do
+        echo "$MODEL"
+        echo "$DATASET"
+        bash scripts/$MODEL/$DATASET.sh        
+    done
+done
