Fixing variance issue

node_classification/models.py
@@ -193,7 +193,7 @@ class GFASTKAN_Nodes(torch.nn.Module):
             self.conv_out = GINConv(make_fastkan(dim_out_message_passing, hidden_channels, num_classes, hidden_layers, grid_size))
         self.dropout = torch.nn.Dropout(dropout)
 
-    def forward(self, x: torch.tensor , edge_index: torch.tensor):
+    def forward(self, x: torch.tensor, edge_index: torch.tensor):
         l = []
         l.append(x)
         for conv in self.convs:

node_classification/optuna_node_classification_fastkan.py
@@ -25,12 +25,15 @@ def objective( trial: Trial,
         hidden_layers = trial.suggest_int('hidden_layers', 1, 4)
     else:
         hidden_layers = trial.suggest_int('hidden_layers', 0, 0)
-    hidden_channels = trial.suggest_int('hidden_channels', 2, 128)
-    lr = trial.suggest_float('lr', 1e-4, 1e-1, log=True)
-    dropout = trial.suggest_float('dropout', 0, 0.9)
-    val_loss = train_and_evaluate_model(hidden_channels, lr, hidden_layers, dropout, data, dataset_name, dataset,
-                               conv_type, skip, grid_size, n_epochs, device)[0]
-    return val_loss
+    hidden_channels = trial.suggest_int('hidden_channels', 4, 128)
+    lr = trial.suggest_float('lr', 1e-5, 1e-2, log=True)
+    dropout = trial.suggest_float('dropout', 0, 0.5)
+    val_losses = []
+    for _ in range(10):
+        val_loss, _, _ = train_and_evaluate_model(hidden_channels, lr, hidden_layers, dropout, data, dataset_name, dataset,
+                                conv_type, skip, grid_size, n_epochs, device)
+        val_losses.append(val_loss)
+    return torch.tensor(val_losses).mean()
 
 def train_and_evaluate_model(hidden_channels: int,
                              lr: float,
@@ -93,7 +96,7 @@ def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     n_epochs = 1000
     skip = False
-    n_trials = 100
+    n_trials = 200
     if not os.path.exists('data'):
         os.makedirs('data')
     set_seed(1)