3c5d11077ff36b65b9fc33ea1c5d023a4bd34fa9
RomanBresson <roman.bresson.5@gmail.com>
2024-11-20 17:54:46 +0100

    Updated GCN

--- node_classification/models.py
+++ node_classification/models.py
@@ -1,5 +1,5 @@
-from ekan import KAN as eKAN
-from fastkan import FastKAN
+from ekan import KAN as eKAN,KANLinear
+from fastkan import FastKAN,FastKANLayer
 import torch
 
 import torch.nn as nn
@@ -30,17 +30,17 @@ def make_fastkan(num_features, hidden_dim, out_dim, hidden_layers, grid_size):
     sizes = [num_features] + [hidden_dim]*(hidden_layers-1) + [out_dim]
     return(FastKAN(layers_hidden=sizes, num_grids=grid_size))
 
-class GCKANLayer(torch.nn.Module):
+class KANLayer(KANLinear):
+    def __init__(self, input_dim, output_dim, grid_size=4, spline_order=3):
+        super(KANLayer, self).__init__(in_features=input_dim, out_features=output_dim, grid_size=grid_size, spline_order=spline_order)
+
+class KAGCNConv(GCNConv):
     def __init__(self, in_feat:int,
                  out_feat:int,
                  grid_size:int=4,
                  spline_order:int=3):
-        super(GCKANLayer, self).__init__()
-        self.kan = eKAN([in_feat, out_feat], grid_size=grid_size, spline_order=spline_order)
-
-    def forward(self, X, A_hat_normalized):
-        X = self.kan(A_hat_normalized @ X)
-        return X
+        super(KAGCNConv, self).__init__(in_feat, out_feat)
+        self.lin = KANLayer(in_feat, out_feat, grid_size, spline_order)
 
 class GIKANLayer(GINConv):
     def __init__(self, in_feat:int,
@@ -52,16 +52,23 @@ class GIKANLayer(GINConv):
         kan = make_kan(in_feat, hidden_dim, out_feat, nb_layers, grid_size, spline_order)
         GINConv.__init__(self, kan)
 
-class GCFASTKANLayer(torch.nn.Module):
+class FKANLayer(FastKANLayer):
+    def __init__(self, input_dim, output_dim, num_grids=4):
+        super(FKANLayer, self).__init__(input_dim=input_dim, output_dim=output_dim, num_grids=num_grids)
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.num_grids = num_grids
+
+    def reset_parameters(self):
+        self.__init__(self.input_dim, self.output_dim, self.num_grids)
+
+class FASTKAGCNConv(GCNConv):
     def __init__(self, in_feat:int,
                  out_feat:int,
                  grid_size:int=4):
-        super(GCFASTKANLayer, self).__init__()
-        self.kan = FastKAN([in_feat, out_feat], num_grids=grid_size)
-
-    def forward(self, X, A_hat_normalized):
-        X = self.kan(A_hat_normalized @ X)
-        return X
+        super(FASTKAGCNConv, self).__init__(in_channels=in_feat, out_channels=out_feat)
+        self.grid_size = grid_size
+        self.lin = FKANLayer(in_feat, out_feat, num_grids=grid_size)
 
 class GIFASTKANLayer(GINConv):
     def __init__(self, in_feat:int,
@@ -132,18 +139,18 @@ class GKAN_Nodes(torch.nn.Module):
         for i in range(mp_layers-1):
             if i ==0:
                 if conv_type == "gcn":
-                    self.convs.append(GCKANLayer(num_features, hidden_channels, grid_size, spline_order))
+                    self.convs.append(KAGCNConv(num_features, hidden_channels, grid_size, spline_order))
                 else:
                     self.convs.append(GIKANLayer(num_features, hidden_channels, grid_size, spline_order, hidden_channels, hidden_layers))
             else:
                 if conv_type == "gcn":
-                    self.convs.append(GCKANLayer(hidden_channels, hidden_channels, grid_size, spline_order))
+                    self.convs.append(KAGCNConv(hidden_channels, hidden_channels, grid_size, spline_order))
                 else:
                     self.convs.append(GIKANLayer(hidden_channels, hidden_channels, grid_size, spline_order, hidden_channels, hidden_layers))
         self.skip = skip
         dim_out_message_passing = num_features+(mp_layers-1)*hidden_channels if skip else hidden_channels
         if conv_type == "gcn":
-            self.conv_out = GCKANLayer(dim_out_message_passing, num_classes, grid_size, spline_order)
+            self.conv_out = KAGCNConv(dim_out_message_passing, num_classes, grid_size, spline_order)
         else:
             self.conv_out = GINConv(make_kan(dim_out_message_passing, hidden_channels, num_classes, hidden_layers, grid_size, spline_order))
         self.dropout = torch.nn.Dropout(dropout)
@@ -175,18 +182,18 @@ class GFASTKAN_Nodes(torch.nn.Module):
         for i in range(mp_layers-1):
             if i ==0:
                 if conv_type == "gcn":
-                    self.convs.append(GCFASTKANLayer(num_features, hidden_channels, grid_size))
+                    self.convs.append(FASTKAGCNConv(num_features, hidden_channels, grid_size))
                 else:
                     self.convs.append(GIFASTKANLayer(num_features, hidden_channels, grid_size, hidden_channels, hidden_layers))
             else:
                 if conv_type == "gcn":
-                    self.convs.append(GCFASTKANLayer(hidden_channels, hidden_channels, grid_size))
+                    self.convs.append(FASTKAGCNConv(hidden_channels, hidden_channels, grid_size))
                 else:
                     self.convs.append(GIFASTKANLayer(hidden_channels, hidden_channels, grid_size, hidden_channels, hidden_layers))
         self.skip = skip
         dim_out_message_passing = num_features+(mp_layers-1)*hidden_channels if skip else hidden_channels
         if conv_type == "gcn":
-            self.conv_out = GCFASTKANLayer(dim_out_message_passing, num_classes, grid_size)
+            self.conv_out = FASTKAGCNConv(dim_out_message_passing, num_classes, grid_size)
         else:
             self.conv_out = GINConv(make_fastkan(dim_out_message_passing, hidden_channels, num_classes, hidden_layers, grid_size))
         self.dropout = torch.nn.Dropout(dropout)
