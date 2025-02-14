f0438a538203435d1f7919f007dbcb54ae9a9e61
RomanBresson <roman.bresson.5@gmail.com>
2025-02-14 09:53:47 +0100

    gat for graph classif - scripts

--- graph_classification/scripts/fastkan/ENZYMES.sh
+++ graph_classification/scripts/fastkan/ENZYMES.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python optuna_graph_classification_fastkan.py --dataset ENZYMES --patience 20 --nb_gnn_layers 4 --epochs 400 --batch-size 64 --random_seed 123
-python optuna_graph_classification_fastkan.py --dataset ENZYMES --patience 20 --nb_gnn_layers 4 --epochs 400 --batch-size 64 --random_seed 1234
-python optuna_graph_classification_fastkan.py --dataset ENZYMES --patience 20 --nb_gnn_layers 4 --epochs 400 --batch-size 64 --random_seed 12345
+python optuna_graph_classification_fastkan.py --dataset ENZYMES --patience 20 --nb_gnn_layers 4 --epochs 400 --batch-size 64 --model_type GAT
+python optuna_graph_classification_fastkan.py --dataset ENZYMES --patience 20 --nb_gnn_layers 4 --epochs 400 --batch-size 64 --model_type GIN
+python optuna_graph_classification_fastkan.py --dataset ENZYMES --patience 20 --nb_gnn_layers 4 --epochs 400 --batch-size 64 --model_type GCN
--- graph_classification/scripts/fastkan/MUTAG.sh
+++ graph_classification/scripts/fastkan/MUTAG.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python optuna_graph_classification_fastkan.py --dataset MUTAG --patience 20 --nb_gnn_layers 2 --epochs 400 --batch-size 32 --random_seed 123
-python optuna_graph_classification_fastkan.py --dataset MUTAG --patience 20 --nb_gnn_layers 2 --epochs 400 --batch-size 32 --random_seed 1234
-python optuna_graph_classification_fastkan.py --dataset MUTAG --patience 20 --nb_gnn_layers 2 --epochs 400 --batch-size 32 --random_seed 12345
+python optuna_graph_classification_fastkan.py --dataset MUTAG --patience 20 --nb_gnn_layers 2 --epochs 400 --batch-size 32 --model_type GAT
+python optuna_graph_classification_fastkan.py --dataset MUTAG --patience 20 --nb_gnn_layers 2 --epochs 400 --batch-size 32 --model_type GIN
+python optuna_graph_classification_fastkan.py --dataset MUTAG --patience 20 --nb_gnn_layers 2 --epochs 400 --batch-size 32 --model_type GCN
