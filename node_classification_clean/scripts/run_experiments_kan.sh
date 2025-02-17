a3c8144507ed31974453f546919f311c034c20dd
RomanBresson <roman.bresson.5@gmail.com>
2025-02-17 09:30:05 +0100

    added gin experiments

--- node_classification_clean/scripts/run_experiments_kan.sh
+++ node_classification_clean/scripts/run_experiments_kan.sh
@@ -13,3 +13,11 @@ python one_experiment.py --dataset Wisconsin --epochs 1000 --patience 50 --conv_
 python one_experiment.py --dataset Cornell --epochs 1000 --patience 50 --conv_type gcn --architecture kan --rate_print 1000
 python one_experiment.py --dataset Actor --epochs 1000 --patience 50 --conv_type gcn --architecture kan --rate_print 1000
 python one_experiment.py --dataset ogbn-arxiv --epochs 1000 --patience 50 --conv_type gcn --architecture kan --rate_print 1000
+
+python one_experiment.py --dataset Cora --epochs 1000 --patience 50 --conv_type gin --architecture kan --rate_print 1000
+python one_experiment.py --dataset CiteSeer --epochs 1000 --patience 50 --conv_type gin --architecture kan --rate_print 1000
+python one_experiment.py --dataset Texas --epochs 1000 --patience 50 --conv_type gin --architecture kan --rate_print 1000
+python one_experiment.py --dataset Wisconsin --epochs 1000 --patience 50 --conv_type gin --architecture kan --rate_print 1000
+python one_experiment.py --dataset Cornell --epochs 1000 --patience 50 --conv_type gin --architecture kan --rate_print 1000
+python one_experiment.py --dataset Actor --epochs 1000 --patience 50 --conv_type gin --architecture kan --rate_print 1000
+python one_experiment.py --dataset ogbn-arxiv --epochs 1000 --patience 50 --conv_type gin --architecture kan --rate_print 1000
